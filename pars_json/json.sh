5c6c2eb9827e030f4460e293ab6f8f92dba9b240
yupirex <yupirex@gmail.com>
2021-12-08 22:32:56 +0200

    Create json.sh

---
+++ pars_json/json.sh
@@ -0,0 +1,28 @@
+!/usr/bin/bash
+jq_string(){ :
+    jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]'
+}
+
+f1(){ :
+    while IFS="=" read -r key value; do
+#        echo "$1$key = $value"
+        json_pars[$1$key]="$value"
+        if jq '.[]' &> /dev/null <<< "$value" ; then
+            < <( <<<"$value" jq_string ) f1 "$1$key."
+        fi
+    done 
+}
+
+declare -A json_pars
+< <( < test_json sed 's/\\n/\\\\n/g' | jq_string ) f1 "."
+
+
+echo -e "${json_pars[.message.text]}"
+Fprint(){ :
+    for key in "${!json_pars[@]}"; do
+        echo "$key = ${json_pars[$key]}"
+    done
+
+}
+#Fprint
+#echo ${!json_pars[@]}
